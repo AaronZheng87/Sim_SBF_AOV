a04f9f3335cbda2818ed98d7db92a4eb9cf15339
AaronZheng87 <aaronzheng87@gmail.com>
2024-03-08 07:30:18 +0100

    check the simulatino 2way aov

--- 1_Stimulation/3_2w*2w_rel.R
+++ 1_Stimulation/3_2w*2w_rel.R
@@ -1,4 +1,6 @@
 library('MASS')
+library(tidyverse)
+library(effectsize)
 library(BayesFactor)
 ############ main effect #################
 ######## 2* 2 with design* #######
@@ -20,14 +22,22 @@ objective_function <- function(initial_mu) {
 }
 
 # 设置初始值
-initial_mu <- c(1, 2, 3, 4)#A1B1, A1B2, A2B1, A2B2
+initial_mu <- c(1, 1, 1, 1)#A1B1, A1B2, A2B1, A2B2
 
 # 使用optim函数最大化目标函数
 result <- optim(par = initial_mu, fn =  function(initial_mu) abs(objective_function(initial_mu) - target_d))
-
+eta_to_d <- function(eta2, N){
+  sqrt(eta2*(N-1)/(N-eta2*N))
+}
 mu <- result$par
 
-N <- 10000
+N <- 100
+iter <- 1000
+d_value <- numeric(iter)
+eta2_value <- numeric(iter)
+eta2_to_d_value <- numeric(iter)
+
+for (i in 1:iter) {
 rho <- cbind(c(1, r, r, r), c(r, 1, r, r), c(r, r, 1, r), c(r, r, r, 1))
 data <- mvrnorm(n=N, mu=mu, Sigma=rho)
 colnames(data) <- c("A1_B1", "A1_B2", "A2_B1", "A2_B2")
@@ -37,13 +47,16 @@ df <- df %>%
   pivot_longer(cols = starts_with("A"), names_to = "group") %>% 
   tidyr::separate(group, c("A", "B"))
   
+cor(data)
 
-
-cohens_d(df$value[df$A == "A1"], df$value[df$A == "A2"], paired = TRUE)
-
-
-
-
-
-
-
+d <- cohens_d(df$value[df$A == "A1"], df$value[df$A == "A2"])$Cohens_d
+model <- afex::aov_ez(id="subj_idx", dv="value", within = c("A", "B"), data=df)
+eta2 <- effectsize::eta_squared(model, partial = TRUE)$Eta2_partial[1]
+d_value[i] = d
+eta2_value[i] = eta2
+eta2_to_d_value[i] = eta_to_d(eta2, 100)
+}
+hist(d_value)
+hist(eta2_value)
+hist(eta2_to_d_value)
+mean(eta2_to_d_value)
